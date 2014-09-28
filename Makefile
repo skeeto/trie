e80058b0e26f85239c9e1efd81a15bb742ccb4b8
Christopher Wellons <wellons@nullprogram.com>
2014-09-28 18:09:29 -0400

    Use intern pool in test.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CFLAGS = -std=c99 -Wall -g3 -O3
 
-test : test.o trie.o
+test : test.o trie.o intern.o
 
 run : test
 	./$^ xylotile < /usr/share/dict/american-english-insane
--- test.c
+++ test.c
@@ -1,20 +1,8 @@
-#define _BSD_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "trie.h"
-
-/* void print(struct trie *trie, int indent) */
-/* { */
-/*     char space[indent * 2 + 1]; */
-/*     memset(space, ' ', sizeof(space) - 1); */
-/*     space[sizeof(space) - 1] = 0; */
-/*     printf("%sTRIE (%d):\n", space, trie->size); */
-/*     for (int i = 0; i < trie->nchildren; i++) { */
-/*         printf("%s  %c:\n", space, trie->children[i].c); */
-/*         print(trie->children[i].trie, indent + 1); */
-/*     } */
-/* } */
+#include "intern.h"
 
 int visitor_print(const char *key, void *data, void *arg)
 {
@@ -22,28 +10,24 @@ int visitor_print(const char *key, void *data, void *arg)
     return 0;
 }
 
-int visitor_free(const char *key, void *data, void *arg)
-{
-    free(data);
-    return 0;
-}
-
 int main(int argc, char **argv)
 {
-    struct trie *trie = trie_create();
+    struct intern pool;
+    if (intern_init(&pool) != 0) {
+        fprintf(stderr, "error: intern pool failure\n");
+        return -1;
+    }
     char line[128];
     while (fgets(line, sizeof(line), stdin)) {
         line[strlen(line) - 1] = '\0';
-        if (trie_insert(trie, line, strdup(line)) != 0) {
+        if (intern(&pool, line) == NULL) {
             printf("error: %s\n", line);
             return 1;
         }
     }
-    printf("%zu words\n", trie_count(trie, ""));
-    char *find = trie_search(trie, argv[1]);
-    printf("'%s'\n", find);
-    trie_visit(trie, "xylot", visitor_print, NULL);
-    trie_visit(trie, "", visitor_free, NULL);
-    trie_free(trie);
+    printf("%zu words\n", trie_count(pool.trie, ""));
+    printf("'%s'\n", (char *) trie_search(pool.trie, argv[1]));
+    trie_visit(pool.trie, "xylot", visitor_print, NULL);
+    intern_free(&pool);
     return 0;
 }
