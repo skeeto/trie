07ac9956ff31db46afd002e415ea44d9a1579294
Christopher Wellons <wellons@nullprogram.com>
2014-09-28 22:12:58 -0400

    More error checking.

--- test.c
+++ test.c
@@ -24,14 +24,20 @@ int main(int argc, char **argv)
     while (fgets(line, sizeof(line), stdin)) {
         line[strlen(line) - 1] = '\0';
         if (intern(&pool, line) == NULL) {
-            printf("error: %s\n", line);
+            printf("error: could not insert '%s'\n", line);
             return 1;
         }
     }
     printf("%zu words\n", intern_count(&pool));
     printf("%0.2f MB\n", trie_size(pool.trie) / 1024.0 / 1024.0);
     printf("'%s'\n", (char *) intern_soft(&pool, argv[1]));
-    trie_visit(pool.trie, "xylot", visitor_print, NULL);
-    intern_free(&pool);
+    if (trie_visit(pool.trie, "xylot", visitor_print, NULL) != 0) {
+        printf("error: visit failure\n");
+        return 1;
+    }
+    if (intern_free(&pool) != 0) {
+        printf("error: free failure\n");
+        return 1;
+    }
     return 0;
 }
